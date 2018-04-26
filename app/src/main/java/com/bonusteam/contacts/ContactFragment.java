03c842e1081978fd736f03405e490f907d495cdc
CarlosDubon <00058016@uca.edu.sv>
2018-04-26 15:54:05 -0600

    Contact Adapter Completado

--- app/src/main/java/com/bonusteam/contacts/ContactFragment.java
+++ app/src/main/java/com/bonusteam/contacts/ContactFragment.java
@@ -7,7 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ContactFragment extends Fragment {
+public class ContactFragment extends android.support.v4.app.Fragment {
     public ContactFragment() {
     }
 
--- app/src/main/java/com/bonusteam/contacts/MainActivity.java
+++ app/src/main/java/com/bonusteam/contacts/MainActivity.java
@@ -1,13 +1,37 @@
 package com.bonusteam.contacts;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
+    private ArrayList<Contacto> contactList = new ArrayList<>();
+    private ContactAdapter contactAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tablayout_id);
+        viewPager = findViewById(R.id.viewpager_id);
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        contactAdapter = new ContactAdapter(contactList);
+        viewPagerAdapter.addFragment(new ContactFragment(),"Contactos");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setElevation(0);
     }
 }
