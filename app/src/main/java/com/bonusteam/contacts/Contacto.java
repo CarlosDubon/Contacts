a2c0772e105fb064f5cd6019f1b29c71fe5dd136
CarlosDubon <00058016@uca.edu.sv>
2018-04-26 11:53:46 -0600

    Clase Contacto Finalizada

--- app/src/main/java/com/bonusteam/contacts/Contacto.java
+++ app/src/main/java/com/bonusteam/contacts/Contacto.java
@@ -1,4 +1,50 @@
 package com.bonusteam.contacts;
 
 public class Contacto {
+    private String name;
+    private String number;
+    private  String email;
+    private String birth;
+
+    public Contacto() {
+    }
+
+    public Contacto(String name, String number, String email, String birth) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        this.birth = birth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
 }
