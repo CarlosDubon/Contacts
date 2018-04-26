99397ecd3dce3a0098b0dc75a0916696fdc459b5
CarlosDubon <00058016@uca.edu.sv>
2018-04-26 11:47:43 -0600

    ContactFragment finalizado -> Creacion de Clase Contacto

--- app/src/main/java/com/bonusteam/contacts/ContactFragment.java
+++ app/src/main/java/com/bonusteam/contacts/ContactFragment.java
@@ -1,4 +1,25 @@
 package com.bonusteam.contacts;
 
-public class ContactFragment {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ContactFragment extends Fragment {
+    public ContactFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_contacts,container,false);
+        return view;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
