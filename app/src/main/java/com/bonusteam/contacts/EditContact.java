cae6167dc7c53e7fe8253b450101c34b9a8867b3
CarlosDubon <00058016@uca.edu.sv>
2018-05-06 10:20:01 -0600

    datepicker editar

--- app/src/main/java/com/bonusteam/contacts/EditContact.java
+++ app/src/main/java/com/bonusteam/contacts/EditContact.java
@@ -1,11 +1,14 @@
 package com.bonusteam.contacts;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
@@ -19,12 +22,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class EditContact extends AppCompatActivity {
     private int PERMISSIONS_READ_EXTERNAL_STORAGE = 97;
@@ -39,6 +44,7 @@ public class EditContact extends AppCompatActivity {
     private int REQUEST_CODE = 1;
     private Uri imageUri;
     private int index;
+    private DatePickerDialog.OnDateSetListener datePickerListener;
 
 
     @Override
@@ -122,6 +128,29 @@ public class EditContact extends AppCompatActivity {
                 startActivityForResult(intent, REQUEST_CODE);
             }
         });
+        birthContact.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    Calendar calendar = Calendar.getInstance();
+                    int year = calendar.get(Calendar.YEAR);
+                    int month = calendar.get(Calendar.MONTH);
+                    int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                    DatePickerDialog dialog = new DatePickerDialog(EditContact.this, android.R.style.Theme_Holo_Light_Dialog, datePickerListener, year, month, day);
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    dialog.show();
+                }
+            }
+        });
+
+        datePickerListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String birthString = year +"-"+month+"-"+dayOfMonth;
+                birthContact.setText(birthString);
+            }
+        };
 
 
     }
